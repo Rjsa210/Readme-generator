273e329983dc0ca5d87528475e8eb6d62cda2cb5
Reid Johnson <rjsa210@gmail.com>
2021-03-26 21:22:29 -0700

    full functionality acheived -RJ

---
+++ Lorem Ipsum_readme.md
@@ -0,0 +1,37 @@
+# Lorem Ipsum
+
+## Description
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor leo a diam sollicitudin tempor id. Suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse. Orci dapibus ultrices in iaculis nunc. 
+
+### Contents
+* [Installation](#Installation)
+* [Usage Information](#Usage-Information)
+* [Liscense](#Liscense)
+* [Contribution Guidelines](#Contributing)
+* [Tests](#Tests)
+* [Questions](#Questions)
+
+
+## Installation 
+Vulputate mi sit amet mauris commodo quis. Integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Varius quam quisque id diam vel quam elementum. Orci a scelerisque purus semper eget duis at tellus at. Habitant morbi tristique senectus et. Congue eu consequat ac felis donec et odio pellentesque diam. Ut sem nulla pharetra diam sit amet nisl suscipit.
+
+## Usage Information
+Cursus mattis molestie a iaculis at erat pellentesque adipiscing. Suspendisse sed nisi lacus sed viverra tellus in hac. Quam nulla porttitor massa id neque aliquam vestibulum morbi blandit. Imperdiet proin fermentum leo vel orci porta non pulvinar neque. Amet consectetur adipiscing elit duis tristique sollicitudin nibh. Eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum. Porta nibh venenatis cras sed felis eget velit aliquet sagittis. Aenean euismod elementum nisi quis eleifend quam adipiscing. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Sed augue lacus viverra vitae. Faucibus interdum posuere lorem ipsum. Pretium lectus quam id leo. Lacus luctus accumsan tortor posuere ac ut consequat semper viverra. Dictumst quisque sagittis purus sit amet volutpat consequat. Id cursus metus aliquam eleifend mi in nulla posuere. Neque viverra justo nec ultrices dui sapien eget mi.
+
+## Liscense
+[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)
+
+
+## Contributing
+Unfortunately, Lorem Ipsum is not accepting contributions at this time. 
+
+## Tests
+Scelerisque purus semper eget duis. Magna ac placerat vestibulum lectus. Maecenas accumsan lacus vel facilisis. Nisl nunc mi ipsum faucibus. Duis at tellus at urna condimentum mattis. Lectus quam id leo in vitae turpis massa. Ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat. In massa tempor nec feugiat. Vehicula ipsum a arcu cursus vitae congue mauris rhoncus. Quam adipiscing vitae proin sagittis. Sit amet mauris commodo quis.
+
+## Questions
+Please send questions to:
+
+Github: [LoremHound](https://github.com/LoremHound)
+
+Email: lorem@ipsum.com
+
--- index.js
+++ index.js
@@ -3,6 +3,47 @@ const fs = require('fs');
 
 
 //create prompts
+//write function to assign badge
+const generateLicenseBadge = (response) => {
+  switch (response.license) {
+    case 'Apache':
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+      break;
+    case 'Boost':
+      return '[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+      break;
+    case 'Eclipse':
+      return '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+      break;
+    case 'IBM Public License':
+      return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)';
+      break;
+    case 'MIT License':
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+      break;
+    case 'Mozilla Public License':
+      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+      break;
+    case 'WTFPL':
+      return '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)';
+      break;
+    default:
+      return '';
+  } 
+}
+
+generateContributions = (response) => {
+  switch (response.contributions) {
+    case 'No contributions':
+      return `Unfortunately, ${response.projectname} is not accepting contributions at this time.`
+      break;
+    case 'Please email suggestions':
+      return `Please send all comments and suggestions via email to: ${response.email}`
+    default:
+      return `${response.projectname} is open source`
+  }
+}
+
 const questions = [
   // What is your project tit
   {
@@ -51,6 +92,10 @@ const questions = [
     name: 'license',
     message: 'Please select your license',
     choices: [
+      'Apache',
+      'Boost',
+      'Eclipse',
+      'IBM Public License',
       'MIT License',
       'Mozilla Public License',
       'WTFPL',
@@ -65,22 +110,22 @@ const questions = [
   //What is your Email address
   {
     type: 'input',
-    name: 'e-mail',
+    name: 'email',
     message: 'What is your E-mail address?'
   },
 ];
 
 
-const generateReadMe = (response) =>
-
-  `# ${response.projectname}
-[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
-
+const generateReadMe = (response) => {
+  let licenseBadge = generateLicenseBadge(response);
+  let contribution = generateContributions(response);
+  return (
+    `# ${response.projectname}
 
 ## Description
-${response.projectname} is intended to ${response.description} ______________. 
+${response.description} 
 
-### Table of Contents
+### Contents
 * [Installation](#Installation)
 * [Usage Information](#Usage-Information)
 * [Liscense](#Liscense)
@@ -96,20 +141,23 @@ ${response.installation}
 ${response.usage}
 
 ## Liscense
-Licensed by the ${response.license} liscense.
+${licenseBadge}
+
 
 ## Contributing
-${response.contributions} 
+${contribution} 
 
 ## Tests
 ${response.test}
 
 ## Questions
-Github: ${response.github}
-Email: ${response.e-mail}
+Please send questions to:\n
+Github: [${response.github}](https://github.com/${response.github})\n
+Email: ${response.email}
 
-`;
-
+`
+  );
+}
 
 
 const init = () => {
@@ -117,12 +165,12 @@ const init = () => {
     .then((response) => {
       try {
         const readme = generateReadMe(response);
-        fs.writeFileSync(`${response.projectname}.md`, readme);
+        fs.writeFileSync(`${response.projectname}_readme.md`, readme);
         console.log('readme successfully created');
       } catch (error) {
         console.log(error);
       }
-});
+    });
 };
 
 init();
